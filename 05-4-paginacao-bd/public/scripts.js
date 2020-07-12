a8967e21e79a4cb2f2705ea9214e17f333158f20
Rafael Martins <rafaeldcmartins@gmail.com>
2020-07-12 16:59:54 -0300

    Frontend done

--- 05-4-paginacao-bd/public/scripts.js
+++ 05-4-paginacao-bd/public/scripts.js
@@ -1,3 +1,4 @@
+// Active page
 const currentPage = location.pathname
 const menuItens = document.querySelectorAll('header .links a')
 
@@ -6,4 +7,60 @@ for (let item of menuItens) {
         item.classList.add('active')
         console.log(item)
     }
+}
+
+// Pagination
+function paginate(selectedPage, totalPages) {
+    let pages = [],
+        previousPage
+
+    for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+        const firstAndLast2pages = currentPage == 1 || currentPage == 2 || currentPage == totalPages - 1 || currentPage == totalPages
+        const pagesBeforeSelectedPage = currentPage >= selectedPage - 1
+        const pagesAfterSelectedPage = currentPage <= selectedPage + 1
+
+        if (firstAndLast2pages || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+            if (previousPage && currentPage - previousPage > 2) {
+                pages.push('...')
+            }
+
+            if (previousPage && currentPage - previousPage == 2) {
+                pages.push(previousPage + 1)
+            }
+
+            pages.push(currentPage)
+            previousPage = currentPage
+        }
+    }
+
+    return pages
+}
+
+function createPagination(pagination) {
+    const page = +pagination.dataset.page
+    const total = +pagination.dataset.total
+    const filter = pagination.dataset.filter
+    const pages = paginate(page, total)
+
+    let elements = ''
+
+    for (let page of pages) {
+        if (String(page).includes('...')) {
+            elements += `<span>${page}</span>`
+        } else {
+            if (filter) {
+                elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+            } else {
+                elements += `<a href="?page=${page}">${page}</a>`
+            }
+        }
+    }
+
+    pagination.innerHTML = elements
+}
+
+const pagination = document.querySelector('.pagination')
+
+if (pagination) {
+    createPagination(pagination)
 }
